a68532cbad132358a157220ec50a0a04a4dc84d1
Badercoder <baders-lastcard@hotmail.com>
2023-07-27 22:22:12 +0400

    menu bar toggle functionality

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 "use strict"
 
+const menuBtn = document.getElementById("menuBtn");
+const header = document.querySelector("header");
+const body = document.querySelector("body");
+
+menuBtn.addEventListener("click", (event) => {
+    document.querySelector("body").classList.toggle("mobile-nav-active");
+    menuBtn.classList.toggle("fa-xmark")
+    event.stopPropagation();
+})
+
+document.addEventListener("click", (event) => {
+    const inSideClick = header.contains(event.target);
+    
+    if(!inSideClick && body.classList.contains("mobile-nav-active")) {
+        document.querySelector("body").classList.toggle("mobile-nav-active");
+        menuBtn.classList.toggle("fa-xmark")
+    }
+})
